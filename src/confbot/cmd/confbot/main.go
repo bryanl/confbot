d360665e17b08546dc79fec54932aa02c762f388
bryanl <bryanliles@gmail.com>
2016-04-24 09:07:16 -0400

    move to envconfig

--- src/confbot/cmd/confbot/main.go
+++ src/confbot/cmd/confbot/main.go
@@ -2,14 +2,13 @@ package main
 
 import (
 	"confbot"
-	"flag"
 	"net/http"
 	"os"
 
 	"golang.org/x/net/context"
 
 	"github.com/Sirupsen/logrus"
-	"github.com/kouhin/envflag"
+	"github.com/kelseyhightower/envconfig"
 
 	"confbot/api"
 	"confbot/logging"
@@ -21,82 +20,70 @@ const (
 )
 
 var (
-	env            = flag.String("confbot-env", "development", "environment")
-	slackToken     = flag.String("confbot-slack-token", "", "slack token")
-	paperTrailHost = flag.String("confbot-papertrail-host", "", "papertrail host")
-	paperTrailPort = flag.Int("confbot-papertrail-port", 0, "papertrail port")
-	doToken        = flag.String("confbot-digitalocean-token", "", "digitalocean token")
-	redisURL       = flag.String("redis-url", "", "redis url")
-	httpAddr       = flag.String("confbot-http-addr", "localhost:8080", "api listen address")
-	remoteLogging  = flag.Bool("confbot-enable-remote-logging", false, "enable remote logging")
-
 	rootLog = logrus.New()
 )
 
+// Specification describes the enviroment required to run confbot.
+type Specification struct {
+	Env               string `default:"development"`
+	SlackToken        string `envconfig:"slack_token" required:"true"`
+	PaperTrailHost    string `envconfig:"papertrail_host"`
+	PaperTrailPort    int    `envconfig:"papertrail_port"`
+	DigitalOceanToken string `envconfig:"digitalocean_token" required:"true"`
+	RedisURL          string `envconfig:"redis_url" required:"true"`
+	HTTPAddr          string `envconfig:"http_addr" default:"localhost:8080"`
+	RemoteLogging     bool   `envconfig:"remote_logging" default:"false"`
+}
+
 func main() {
-	envflag.Parse()
+	var spec Specification
+	err := envconfig.Process("confbot", &spec)
+	if err != nil {
+		rootLog.WithError(err).Fatal("unable to read environment")
+	}
 
 	log := rootLog.WithFields(logrus.Fields{
-		"env": *env,
+		"env": spec.Env,
 	})
 
-	setupLogger(log)
-	verifyEnv(log)
+	setupLogger(spec, log)
 
 	ctx := context.WithValue(context.Background(), "log", log)
 
-	s, err := slack.New(ctx, *slackToken)
+	s, err := slack.New(ctx, spec.SlackToken)
 	if err != nil {
 		rootLog.WithError(err).Fatal("unable to connect to slack")
 	}
 
 	log.Info("application started")
 
-	repo, err := confbot.NewRepo(ctx, *redisURL, *env)
+	repo, err := confbot.NewRepo(ctx, spec.RedisURL, spec.Env)
 	if err != nil {
 		rootLog.WithError(err).Fatalf("unable to create repo")
 	}
 
 	cb := confbot.New(ctx, s, repo)
 
-	cb.AddTextAction("./boot shell", confbot.CreateBootShellAction(ctx, *doToken, repo))
+	cb.AddTextAction("./boot shell", confbot.CreateBootShellAction(ctx, spec.DigitalOceanToken, repo))
 	go cb.Listen()
 
 	a := api.New(ctx, repo, s)
 	http.Handle("/", a.Mux)
 
-	log.WithField("addr", *httpAddr).Info("created http server")
-	log.Fatal(http.ListenAndServe(*httpAddr, nil))
+	log.WithField("addr", spec.HTTPAddr).Info("created http server")
+	log.Fatal(http.ListenAndServe(spec.HTTPAddr, nil))
 }
 
-func verifyEnv(log *logrus.Entry) {
-	if *slackToken == "" {
-		log.Fatalf("CONFBOT_SLACK_TOKEN environment variable is required")
-	}
-
-	if *remoteLogging && *paperTrailHost == "" {
-		log.Fatalf("CONFBOT_PAPERTRAIL_HOST environment variable is required")
-	}
-
-	if *remoteLogging && *paperTrailPort == 0 {
-		log.Fatalf("CONFBOT_PAPERTRAIL_PORT environment variable is required")
-	}
-
-	if *redisURL == "" {
-		log.Fatalf("REDIS_URL environment variable is required")
-	}
-}
-
-func setupLogger(log *logrus.Entry) {
-	if *remoteLogging {
+func setupLogger(spec Specification, log *logrus.Entry) {
+	if spec.RemoteLogging {
 		hostname, err := os.Hostname()
 		if err != nil {
 			log.Fatalf("unable to retrieve app hostname: %v", err)
 		}
 
 		hook, err := logging.NewPapertrailHook(&logging.Hook{
-			Host:     *paperTrailHost,
-			Port:     *paperTrailPort,
+			Host:     spec.PaperTrailHost,
+			Port:     spec.PaperTrailPort,
 			Hostname: hostname,
 			Appname:  "confbot",
 		})
